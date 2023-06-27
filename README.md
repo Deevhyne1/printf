6c4b274ee60eae833bb9f32d0859827ace58eab8
deevhyne1 <divinechukwueme77@gmail.com>
2023-06-27 23:11:31 +0100

    printf

---
+++ README.md
@@ -0,0 +1,5 @@
+PRINTF
+
+This is an alx se group project 
+presented by 
+Eke Diivne and Miracle
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c) __attribute__((unused));
+
+#endif /* MAIN_H */
